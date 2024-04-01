283f28a256554460bafdb67c365aac591889d5ff
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-03-31 19:24:16 -0500

    updated tasks app models

--- taskboard/tasks/models.py
+++ taskboard/tasks/models.py
@@ -2,60 +2,146 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 
-class Task(models.Model):
-    class Category(models.TextChoices):
-        NONE          = "NONE", _("None")
-        GAMES         = "GAME", _("Games")
-        WEBSITES      = "SITE", _("Websites")
-        APPS          = "APPS", _("Apps")
-        MISCELLANEOUS = "MISC", _("Miscellaneous")
 
+class Project(models.Model):
+    """A project is an existing application.
+    
+    Tasks which themselves aren't suggestions for new projects will be
+    connected to an existing project."""
+
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    """The name of the project."""
+
+    summary = models.CharField(
+        max_length=255,
+        blank=True,
+        default="No summary provided.",
+    )
+    """An overview of a project."""
+
+
+class Task(models.Model):
+    """A task is work that needs to be done.
+    
+    In the context of the Taskboard, a task is a job for Rovi to look
+    into and perform.
+    """
     class Type(models.TextChoices):
-        NONE     = "NONE", _("None"),
-        CONCEPT  = "CNPT", _("Concept")
-        IDEA     = "IDEA", _("Idea")
-        ISSUE    = "ISSU", _("Issue")
-        UNSORTED = "UNST", _("Unsorted")
+        """Enumeration for task types."""
+
+        TASK    = "TASK", _("Task"),
+        """Represents any task. Can be used as a default if no task
+        fits, or the author is unsure of what task type to use."""
+
+        FEATURE = "FEAT", _("Feature"),
+        """Feature request for an existing project."""
+
+        PROJECT = "PROJ", _("Project")
+        """Idea for a new project."""
+
+        ISSUE   = "ISSU", _("Issue")
+        """Bug or issue with existing project."""
+
+        UNKNOWN = "UNKN", _("UNKNOWN")
+        """Default value if task type wasn't provided."""
 
     class Status(models.TextChoices):
-        TODO        = 'TODO', _("To Do")
-        REJECTED    = 'NOPE', _("Rejected")
-        CANCELLED   = 'STOP', _("Cancelled")
-        IN_PROGRESS = 'WORK', _("In Progress")
-        REVIEWING   = 'REVW', _("Reviewing")
-        COMPLETE    = 'DONE', _("Complete")
+        """Enumeration for the status of a task."""
 
-    title = models.CharField(
-        max_length=200,
+        TODO        = 'TODO', _("To Do")
+        """Default value. Task is part of to do list."""
+
+        IN_PROGRESS = 'WORK', _("In Progress")
+        """Task is being worked on."""
+
+        REVIEWING   = 'REVW', _("Reviewing")
+        """Task is considered complete, but needs to be reviewed."""
+
+        COMPLETE    = 'DONE', _("Complete")
+        """Task is complete."""
+
+        REJECTED    = 'NOPE', _("Rejected")
+        """Task has been rejected.
+        
+        Used when a task is not deemed suitable to be on the
+        taskboard. Possible reasons for rejection include:
+
+        - Not enough information,
+        - Impossible to do,
+        - or duplicate of another task.
+        """
+
+        CANCELLED   = 'STOP', _("Cancelled")
+        """Task has been cancelled, and cannot be completed.
+        
+        This status is used when an attempt to complete the task has
+        been made, but it cannot or will not be completed.
+        """
+    
+    summary = models.CharField(
+        max_length=255, # same length as Jira summary length
         blank=True,
-        default="No title provided.",
+        default="No summary provided.",
     )
+    """Short description of task. Ideally a single sentence."""
 
     description = models.CharField(
-        max_length=1000,
+        # 255 * 16 because i am not doing Jira's 32,767 characters
+        max_length=4080,
         blank=True,
         default="No description provided."
     )
+    """Long description of a task. If the task is an issue, this will
+    explain the details of the problem; if it's a feature, it'll go
+    into what the feature entails."""
 
-    poster = models.ForeignKey(
+    author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
     )
+    """The user that created the task."""
 
-    category = models.CharField(
-        max_length=4,
-        choices=Category,
-        default=Category.NONE,
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        blank=True,
+        default=None,
     )
+    """The project that a task is connected to.
+    
+    This should always be null if the task type is Project, or at
+    least treated as null.
+    """
 
     type = models.CharField(
         max_length=4,
         choices=Type,
-        default=Type.NONE,
+        default=Type.UNKNOWN,
     )
+    """Type of task."""
 
     status = models.CharField(
         max_length=4,
         choices=Status,
         default=Status.TODO,
     )
+    """Progress of task."""
+
+    status_comment = models.CharField(
+        max_length=255,
+        blank=True,
+        default="Task successfuly created.",
+    )
+    """Optional field for explaining the reason for a status.
+    
+    Expected to be used for status's that are typically the final
+    status for a task: Complete, Rejected, and Cancelled.
+    """
+
+    date_created = models.DateField(
+        auto_now_add=True
+    )
+    """Date task was created."""
