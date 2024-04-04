d3453d20a7afb0bd064efac4ca8c3d8acb6136fd
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-04-03 23:03:04 -0500

    added list and retrieve for tasks

--- taskboard/taskboard/urls.py
+++ taskboard/taskboard/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from tasks import views
+
+router = DefaultRouter()
+
+router.register(r"tasks", views.TaskViewSet, basename='tasks')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('users.urls')),
+    path("api/", include(router.urls)),
 ]
--- taskboard/tasks/views.py
+++ taskboard/tasks/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .serializers import TaskOverviewSerializer, TaskDetailsSerializer
+from .models import Task
+
 
 # Create your views here.
+
+class TaskViewSet(viewsets.ViewSet):
+    """
+    CRUD Functionality related to Tasks.
+    """
+
+    queryset = Task.objects.all()
+    lookup_field = "uuid"
+
+    def list(self, request):
+        serializer = TaskOverviewSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, uuid=None):
+        task = get_object_or_404(self.queryset, uuid=uuid)
+        serializer = TaskDetailsSerializer(task)
+        return Response(serializer.data)
