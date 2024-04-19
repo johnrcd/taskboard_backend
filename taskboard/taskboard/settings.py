fa2f46610753a99f2f3ca08a6ec8e97cdc5f97b1
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-04-19 14:47:40 -0500

    updated settings.py

--- taskboard/taskboard/settings.py
+++ taskboard/taskboard/settings.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "nn9kvlph+#z$c2qr@s2!5y)59dtb(0xr$y7!r0(fo7t*a8w=w"
 
 DEBUG = True
 
