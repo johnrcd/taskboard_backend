36d6c162d3575b2090072ff9a0bbc9dc3300c8c3
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-07-15 20:59:45 -0500

    updated user admin to show profile fields

--- taskboard/users/admin.py
+++ taskboard/users/admin.py
@@ -3,6 +3,8 @@ from django.contrib.auth.admin import UserAdmin
 from .models import TaskboardUser
 
 class TaskboardUserAdmin(UserAdmin):
-    pass
+    fieldsets = UserAdmin.fieldsets + (
+        ("Profile", {"fields": ("name", "title", "about_me")}),
+    )
 
 admin.site.register(TaskboardUser, TaskboardUserAdmin)
